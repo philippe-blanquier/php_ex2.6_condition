e436742d0c41ddccb244e61e872e379244e1ff1f
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 13:12:52 +0200

    creation

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <title>PHP ex2.6 condition</title>
+    </head>
+    <body >
+        <?php
+        $age=21;
+        if( $age >= 18)
+        {
+            echo ("Tu es majeur");
+        }
+        else
+        {
+            echo ("Tu n'est pas majeur");
+        }
+       ?>
+    </body>
+</html>
